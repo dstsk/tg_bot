4d147d49d7a3d791c6891da6fa8eee554f881ef1
dstsk <dotsenko.tsk@gmail.com>
2024-09-08 23:15:59 +0700

    fix deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     return (
     <div className="App">
       work
-        <button>Закрыть</button>
+        <button onClick={() => onClose()}>Закрыть</button>
     </div>
     );
 }
